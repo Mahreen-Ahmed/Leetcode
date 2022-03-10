c81d1cc92ce83e1c4287a2154b3877c9fbfce3e7
Mahreen Ahmed <88930659+Mahreen-Ahmed@users.noreply.github.com>
2022-03-10 05:29:36 -0800

    Time: 0 ms (100.00%), Space: 6.6 MB (52.23%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+       int count=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==' ' && count>0)return count;
+            else if(s[i]!=' ')count++;
+        }
+        return count;
+    }
+};
