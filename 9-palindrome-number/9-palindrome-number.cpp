b50012cce1e6dac15bbf161391352e01ce9a4091
Mahreen Ahmed <88930659+Mahreen-Ahmed@users.noreply.github.com>
2022-03-23 02:01:20 -0700

    Time: 11 ms (83.29%), Space: 5.9 MB (33.36%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -3,12 +3,12 @@ public:
     bool isPalindrome(int x) {
         if(x<0)
             return false;
-       long int rev=0;
-       int num=x,digit=0;
-        while(num!=0){
-            digit=num%10;
+     unsigned long int rev=0;
+        int n=x,  digit=0;
+        while(n!=0){
+            digit=n%10;
             rev=(rev*10)+digit;
-            num=num/10;
+            n=n/10;
             
         }
         if(rev==x)
