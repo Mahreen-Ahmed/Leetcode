780191e7976b3d08a7badffdb8cadcc4c641b534
Mahreen Ahmed <88930659+Mahreen-Ahmed@users.noreply.github.com>
2022-04-08 03:22:53 -0700

    Time: 16 ms (92.47%), Space: 18.1 MB (20.19%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+      int assumption=nums.size()*(nums.size()+1)/2;
+        int total=0;
+        for(int i=0;i<nums.size();i++){
+            total+=nums[i];
+        }
+        return assumption-total;
+        
+    }
+};
